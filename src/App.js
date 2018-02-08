1da8c887f77587eec37b50378cc259c53ec1e38f
Sylvance <kerandisylvance@gmail.com>
2018-02-08 13:42:19 +0300

    Add CI and heroku configurations

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Yummy Recipes</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="App-intro">
+          Yummy recipes provides a platform for users to keep track of their awesome 
+          recipes and share with others if they so wish.
+        </div>
       </div>
     );
   }
